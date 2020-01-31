11111f510c065aa2cede601d31d4e271fae83d22
Dan Jiang <danjiang5956@gmail.com>
2020-01-31 15:55:25 +0800

    first commit

---
+++ DTLiving/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  DTLiving
+//
+//  Created by Dan Jiang on 2020/1/31.
+//  Copyright © 2020 Dan Jiang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window?.rootViewController = CameraViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ DTLiving/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  DTLiving
+//
+//  Created by Dan Jiang on 2020/1/31.
+//  Copyright © 2020 Dan Jiang. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
